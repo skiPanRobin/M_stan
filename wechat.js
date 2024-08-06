51635e2729017cca3dd2213e5dad9d360661bfae
huangfb04 <huangfb04@vanke.com>
2024-08-06 18:14:32 +0800

    添加状态信息

--- wechat.js
+++ wechat.js
@@ -1,14 +1,19 @@
 auto();
 const { actionSleep, pressSleep, autoSwipe, pressXY, descClick } = require('./utils')
 
-function openWechat(payload){
+function _openWechat(payload){
     actionSleep(back, 50);
     actionSleep(back, 100)
     actionSleep(home, 100)
     actionSleep(home, 500)
     // 打开微信应用
     var wechatText =  payload.wechatNo === 1? '微信' : '工作微信'
-    descClick(wechatText, 500)
+    if (desc(wechatText).findOne()){
+        descClick(wechatText, 500)
+    } else {
+        console.error(`打开微信失败, ` + JSON.stringify(payload))
+        return {'status': 2, "message": '打开微信失败'}
+    }
     
     // 账号切换(选择账号)
     var me = className('android.widget.TextView').text('我').findOne(5000).parent()
@@ -19,14 +24,18 @@ function openWechat(payload){
         pressSleep("我", 500);
     }
     // 判断当前账号是否需要切换
-    if (className('android.view.View').textContains(payload.wechatName).findOne(3000)){
+    if (className('android.view.View').textContains(payload.wechatName).findOne(1000)){
         console.log(`当前账号为目标账号, 不需要切换: ${payload.wechatName}`)
         sleep(200)
     } else{
         pressSleep("设置", 500);
         autoSwipe(1037, 2068, 1024, 1037, 200, 500)
         pressSleep('切换账号', 500)
-        pressSleep(payload.wechatName, 500)
+        if(!pressSleep(payload.wechatName, 500)){
+            console.log('切换账号失败: ' + JSON.stringify(payload));
+            return {"status": 3, "message": "无法查找到微信名, 切换微信账号失败"}
+        }
+        
         // 返回
         var button返回 = className('android.widget.ImageView').desc('返回').findOne(8000)
         if (button返回 && button返回.bounds().centerX() > 0 && button返回.bounds().centerY()>0){
@@ -35,6 +44,7 @@ function openWechat(payload){
         } else{
             actionSleep(back, 500)
         }
+        return {"status": 0, "message": "openWechat"}
     }
 
     var parent微信 = className('android.widget.TextView').text('微信').findOne(5000).parent()
@@ -46,6 +56,38 @@ function openWechat(payload){
     }
     autoSwipe(500, 1037, 520, 2568, 300, 100)
     toast("微信切换完成")
+}
+
+/**
+ * @param payload -任务信息
+ * status: 0 正常; 1 - 9 为微信启动异常
+*/
+function openWechat(payload){
+    var msg = {
+        'type': 'errorMsg',
+        'status': 0,      
+        'message': 'openWechat',
+        'payload': {
+            'id': payload.id,
+            "city": payload.city,
+            "shopName": payload.shopName,
+            "wechatNo": payload.wechatNo,
+            "wechatName": payload.wechatName,
+            "shopList": payload.shopList
+        }
+    }
+    try {
+        r = _openWechat(payload)
+        msg.status = r.status
+        msg.message = r.message
+    } catch (error) {
+        error.to
+        msg.status = 1
+        msg.message = error.message
+    }
+    console.log(JSON.stringify(msg));
+    return msg
+    
 }
 
 module.exports = {
