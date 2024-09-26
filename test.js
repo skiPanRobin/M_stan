a19a706148e7ad41275f38566349572a8b80bb73
huangfb04 <huangfb04@vanke.com>
2024-09-26 18:21:23 +0800

    ocr测试

--- test.js
+++ test.js
@@ -94,127 +94,28 @@ console.log(openWechat(payload));
 
 console.log(mstand(payload));
 
-// console.log(text('同意').findOne(100).text());
+auto.waitFor();
+images.requestScreenCapture()
+var xy门店自取 = [190, 1120, 450 , 1230]
+var ex异常判断 = [320, 700, 800, 1600]
 
+function ocrLoctionXY(img, xy, checkText){
 
-
-// textContains('张可用').findOne(500)
-// var couponsEle = textContains('张可用').findOne(500)
-// pressXY(505, 1390, 150, 500)
-// click(textContains("单杯标杯饮品兑换券").findOne(8000).bounds().centerX(), textContains("单杯标杯饮品兑换券").findOne(8000).bounds().centerY())
-// textContains('单杯标杯饮品兑换券').findOne(8000)
-// sleep(1000)
-// click(textContains("单杯标杯饮品兑换券").findOne(8000).bounds().centerX(), textContains("单杯标杯饮品兑换券").findOne(8000).bounds().centerY())
-
-// descContains('“工作微信”').findOne(100).click()
-// openWechat(payload)
-// mstand(payload)
-// mstandSelectDrinks(payload)
-// pressSleep('去下单', 100)
-
-// var remarksInput = text('如有忌口过敏请填写到这儿').findOne(3000)
-// console.log(remarksInput.bounds().centerY());
-
-// console.log(getAppName('com.mstand.autox'));
-// console.log(getPackageName('微信'))
-
-// 获取 mainActivity
-// console.log(shell("dumpsys package com.mstand.autox| grep -A 1 MAIN", true));
-// 使用shell启动应用
-// console.log(shell("am start -n com.mstand.autox/com.stardust.auojs.inrt.SplashActivity;", true));
-
-// console.log(currentPackage() === 'com.tencent.mm')
-// app.launchPackage('com.tencent.mm')
-
-// var shotPath = "/sdcard/Pictures/screenshot.png";
-// function takeScreenShot() {
-//     sleep(200)
-//     var result = shell("screencap -p " + shotPath, true);
-//     if (result.code == 0) {
-//         console.log("截图成功，保存路径：" + shotPath);
-//     } else {
-//         console.log("截图失败");
-//     }
-// };
-// takeScreenShot()
-// console.log(text('自提').findOne(100).bounds().centerX());
-// if ((!!text('确认下单').findOne(2000) === false) && (!!text('去结算').findOne(1000) === false)){
-//     descClick('返回', 100)
-//     if (text('去结算').findOne(2000)){
-//         sleep(200)
-//         text('去结算').findOne(2000).click()
-//     } else {
-//         throw new Error('无法定位确认下单, 点击返回后, 无法定位去结算')
-//     }
-
-// }currentPackage() ==  'com.autox.mstandauto'
-// console.log(currentPackage());
-// home()
-// console.log(currentActivity());
-// console.log(launchPackage('com.autox.startmstandauto'));
-// forceStopApp('com.tencent.mm.plugin.appbrand.ui.AppBrandUI00')
-// shell('am force-stop ' + 'com.tencent.mm', true)
-// var packageName = 'com.autox.mstandauto'
-// shell('am force-stop ' + packageName, true)
-// swipe(400, 800, 410, 400, 400)
-// sleep(300)
-// click(ele.bounds().centerX(), ele.bounds().centerY())
-// var productName = '拿铁'
-// var categroy = '黑咖'
-
-// pressSleep(categroy, 500)
-// while (true){
-//     var centerY = text(productName).findOne(1000).bounds().centerY()
-//     console.log(centerY);
-//     if (800 < centerY &&  centerY <  2000){
-//         console.log(text(productName).findOne(1000).bounds().centerY());
-//         pressSleep(productName)
-//         break
-//     } else {
-//         autoSwipe(400, 1800, 400, 500, 600, 1000)
-//     }
-// }
-
-// text('规格').findOne(2000)
-// autoSwipe(400, 1300, 400, 500, 300, 300)
-// var ele = text('标杯（热）295').findOne(100)
-// pressContainsSleep('标杯（热）295')
-// pressContainsSleep('加份浓度')
-// pressContainsSleep('燕麦奶')
-// pressSleep('加入购物车')
-// function toastXY(str){
-//     var ele = text(str).findOne(1000)
-//     if (ele) {
-//         var bounds = ele.bounds()
-//         console.log(`X: ${bounds.centerX()}; Y: ${bounds.centerY()}`)
-//     } else {
-//         toast('定位不到: ', str)
-//     }
-// }
-
-
-// _newWriteNotes('能下单吗')
-// console.log(text('如有忌口过敏请填写到这儿').findOne(3000).click());
-// toastXY('备注')
-// pressSleep('去结算', 500)
-// var 下单ele = text('确认下单').findOne(2000)
-// if (下单ele){
-//     var bounds = 下单ele.bounds()
-// }
-// var notes = '不加咖啡'
-// if (!!notes) {
-//     setClip(notes);
-//     sleep(300)
-//     autoSwipe(400, 1300, 400, 500, 300, 500)    // 滑动到底部
-//     pressXY(700, 1950, 200, 500)                // 点击备注输入框
-//     press(600, 1000, 300)                       // 点击输入法输入框
-//     var finish = text('完成').findOne(5000) 
-//     click(device.width/2, finish.bounds().bottom + 80)  // 点击输入法剪贴板上备注
-//     sleep(200)
-//     pressSleep('完成', 400)
-// }
-
-// pressXY(523, 1750, 200, 600)  // 点击备注框'保存'按钮
-// pressXY(bounds.centerX(), bounds.centerY(), 200, 500)   // 点击确认下单
-// pressSleep('确定门店', 500)
-
+    var clipImg = images.clip(img, xy[0], xy[1], xy[2] - xy[0], xy[3]-xy[1])
+    var gimg = images.grayscale(clipImg)
+    var gimg = images.threshold(gimg, 100, 255, "BINARY")
+    var res = paddle.ocr(gimg)
+    for (let index = 0; index < res.length; index++) {
+        const ocrResult = res[index];
+        if (ocrResult.text==checkText){
+            console.log(`定位 "${checkText}" 成功`);
+            return [xy[0] + ocrResult.bounds.centerX(), xy[1] + ocrResult.bounds.centerY()]
+        }
+    }
+    console.log(`定位 "${checkText}" 失败`);
+    
+}
+var img = images.captureScreen()
+var [cx, cy]  = ocrLoctionXY(img, xy门店自取, '门店自取')
+console.log(`cx: ${cx}, cy:${cy}`);
+click(cx, cy)
