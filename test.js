17b808acec142c37db2c4944e1618c4cf1fd9e9a
huangfb04 <huangfb04@vanke.com>
2024-09-27 18:01:43 +0800

    ...

--- test.js
+++ test.js
@@ -62,27 +62,26 @@ const { pressXY, pressSleep } = require('./utils')
 // }
 
 var payload = {
-    "id": "1257",
-    "city": "北京市",
+    "id": "1259",
+    "city": "深圳市",
     "notes": "",
-    "isTest": false,
+    "isTest": true,
     "appName": "M Stand",
     "coupons": {
         "total": 1,
         "titleSub": "单杯标杯饮品兑换券"
     },
     "shopList": [{
-        "feature": ["标杯（冰）354ml", "少冰"],
-        "category": "零咖特饮",
+        "feature": ["标杯（冷）354ml", "正常浓度"],
+        "category": "果咖",
         "quantity": 1,
-        "productName": "青麦草"
+        "productName": "冰摇香橙美式"
     }],
-    "shopName": "北京乐成中心店",
+    "shopName": "深圳东海缤纷天地店",
     "wechatNo": 1,
     "orderType": "店内就餐",
     "wechatName": "阿呆的大哥"
 }
-
 // var shopName = '上海世纪汇广场店'
 // pressSleep('请输入门店名称', 500)
 // var inputEle = text('请输入门店名称').findOne(1000)
@@ -94,28 +93,28 @@ console.log(openWechat(payload));
 
 console.log(mstand(payload));
 
-auto.waitFor();
-images.requestScreenCapture()
-var xy门店自取 = [190, 1120, 450 , 1230]
-var ex异常判断 = [320, 700, 800, 1600]
+// auto.waitFor();
+// images.requestScreenCapture()
+// var xy门店自取 = [190, 1120, 450 , 1230]
+// var ex异常判断 = [320, 700, 800, 1600]
 
-function ocrLoctionXY(img, xy, checkText){
+// function ocrLoctionXY(img, xy, checkText){
 
-    var clipImg = images.clip(img, xy[0], xy[1], xy[2] - xy[0], xy[3]-xy[1])
-    var gimg = images.grayscale(clipImg)
-    var gimg = images.threshold(gimg, 100, 255, "BINARY")
-    var res = paddle.ocr(gimg)
-    for (let index = 0; index < res.length; index++) {
-        const ocrResult = res[index];
-        if (ocrResult.text==checkText){
-            console.log(`定位 "${checkText}" 成功`);
-            return [xy[0] + ocrResult.bounds.centerX(), xy[1] + ocrResult.bounds.centerY()]
-        }
-    }
-    console.log(`定位 "${checkText}" 失败`);
+//     var clipImg = images.clip(img, xy[0], xy[1], xy[2] - xy[0], xy[3]-xy[1])
+//     var gimg = images.grayscale(clipImg)
+//     var gimg = images.threshold(gimg, 100, 255, "BINARY")
+//     var res = paddle.ocr(gimg)
+//     for (let index = 0; index < res.length; index++) {
+//         const ocrResult = res[index];
+//         if (ocrResult.text==checkText){
+//             console.log(`定位 "${checkText}" 成功`);
+//             return [xy[0] + ocrResult.bounds.centerX(), xy[1] + ocrResult.bounds.centerY()]
+//         }
+//     }
+//     console.log(`定位 "${checkText}" 失败`);
     
-}
-var img = images.captureScreen()
-var [cx, cy]  = ocrLoctionXY(img, xy门店自取, '门店自取')
-console.log(`cx: ${cx}, cy:${cy}`);
-click(cx, cy)
+// }
+// var img = images.captureScreen()
+// var [cx, cy]  = ocrLoctionXY(img, xy门店自取, '门店自取')
+// console.log(`cx: ${cx}, cy:${cy}`);
+// click(cx, cy)
